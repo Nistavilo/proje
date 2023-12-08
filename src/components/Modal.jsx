979e1ac21e3deb3fc69fa94c378ea77442fdf9ee
nistavilo <emirhankorkmaz275@gmail.com>
2023-12-08 23:15:34 +0300

    input

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { IoMdClose } from "react-icons/io";
+import Input from "./Input";
 
+const Modal = ({title, content, btnText, btnFunc}) => {
+    const onChangeFunc = (e) => {
 
-const Modal = () => {
+    }
     return(
-        <div>Modal</div>
+        <div className="fixed top-0 left-0 bottom-0 right-0 w-full h-screen flex items-center justify-center">
+            <div className="w-1/3 bg-white shadow-lg rounded-m p-4">
+                <div className="border-b py-3 flex items-center justify-between">
+                    <div className="text-2xl"> {title} </div>
+                    <IoMdClose size={24}/>
+                </div>
+                <Input placeholder={"Ürün ekle"} name={"name"} id={"name"} onChange={onChangeFunc}    />
+            </div>
+        </div>
     )
 }
 
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,11 +6,14 @@ import Modal from "../components/Modal";
 const Product = () => {
     const {modal}= useSelector( state=> state.modal)
     console.log(modal,"modal")
+    const buttonFunc = () =>{}
     return (
         <div>
            <ProductCard/>
-           {modal && <Modal/>} 
-        </div>)
+           {modal && <Modal title = {"Ürün oluştur."} btnText = {"Oluştur"} btnFunc = {buttonFunc} />} 
+        </div>
+
+        )
 }
 
-export default Product
+export default Product 
