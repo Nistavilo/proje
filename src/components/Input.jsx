4d975e52d90f80bc84480ece8028638c00b0ca38
nistavilo <emirhankorkmaz275@gmail.com>
2023-12-09 18:49:35 +0300

    commit2

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 
-const Input = ({placeholder,type,id,name,onChange}) => {
+const Input = ({placeholder,type,id,name,onChange, value}) => {
     return(
-        <input className="h-10 w-full border rounded-md p-2 outline-none mt-3" placeholder={placeholder} type={type} id={id} name={name} onChange={onChange}/>
+        <input value={value} className="h-10 w-full border rounded-md p-2 outline-none mt-3" placeholder={placeholder} type={type} id={id} name={name} onChange={onChange}/>
     )
 }
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { IoMdClose } from "react-icons/io";
 import Input from "./Input";
+import { useState } from "react";
 
 const Modal = ({title, content, btnText, btnFunc}) => {
     const [productInfo, setProductInfo] = useState({name: "",price:"", url:""})
-    const onChangeFunc = (e) => {
-        if(type== "url"){
+    const onChangeFunc = (e,type) => {
+        if(type == "url"){
             setProductInfo(prev => ({...prev, [e.target.name]: URL.createObjectURL(e.target.files[0])}))
         }else{
             setProductInfo(prev => ({...prev, [e.target.name]: e.target.value}))
 
         }
     }
+    console.log(productInfo,"productInfo")
     return(
         <div className="fixed top-0 left-0 bottom-0 right-0 w-full h-screen flex items-center justify-center">
             <div className="w-1/3 bg-white shadow-lg rounded-m p-4">
