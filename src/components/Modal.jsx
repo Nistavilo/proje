2f3b2e3875387b3bd03c1ce71dbfcbdee07fbf01
nistavilo <emirhankorkmaz275@gmail.com>
2023-12-10 00:02:14 +0300

    commit1

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,20 +1,8 @@
 import React from "react";
 import { IoMdClose } from "react-icons/io";
-import Input from "./Input";
-import { useState } from "react";
-import Button from "./Button";
 
 const Modal = ({title, content, btnText, btnFunc}) => {
-    const [productInfo, setProductInfo] = useState({name: "",price:"", url:""})
-    const onChangeFunc = (e,type) => {
-        if(type == "url"){
-            setProductInfo(prev => ({...prev, [e.target.name]: URL.createObjectURL (e.target.files[0])}))
-        }else{
-            setProductInfo(prev => ({...prev, [e.target.name]: e.target.value}))
 
-        }
-    }
-    console.log(productInfo,"productInfo")
     return(
         <div className="fixed top-0 left-0 bottom-0 right-0 w-full h-screen flex items-center justify-center">
             <div className="w-1/3 bg-white shadow-lg rounded-m p-4">
@@ -22,10 +10,7 @@ const Modal = ({title, content, btnText, btnFunc}) => {
                     <div className="text-2xl"> {title} </div>
                     <IoMdClose size={24}/>
                 </div>
-                <Input type={"text"} placeholder={"Ürün ekle"}  name={"name"}  id={"name"}  onChange={e=> onChangeFunc(e,"name")}    />
-                <Input type={"text"} placeholder={"Fiyat ekle"} name={"price"} id={"price"} onChange={e=> onChangeFunc(e,"price")}    />
-                <Input type={"file"} placeholder={"Resim seç"}  name={"url"}   id={"url"}   onChange={e=> onChangeFunc(e,"file")}    />
-                <Button btnText={btnText} onClick={btnFunc} />
+                {content}
             </div>
         </div>
     )
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,15 +2,33 @@ import React from "react";
 import ProductCard from "../components/ProductCard";
 import { useSelector } from "react-redux";
 import Modal from "../components/Modal";
+import Button from "../components/Button";
+import Input from "../components/Input"
 
-const Product = () => {
+const Product = () => {    
     const {modal}= useSelector( state=> state.modal)
+    const [productInfo, setProductInfo] = useState({name: "",price:"", url:""})
+    const onChangeFunc = (e,type) => {
+        if(type == "url"){
+            setProductInfo(prev => ({...prev, [e.target.name]: URL.createObjectURL (e.target.files[0])}))
+        }else{
+            setProductInfo(prev => ({...prev, [e.target.name]: e.target.value}))
+         }
+    }
     console.log(modal,"modal")
     const buttonFunc = () =>{}
+    const contentModal = (
+        <>
+            <Input type={"text"} placeholder={"Ürün ekle"}  name={"name"}  id={"name"}  onChange={e=> onChangeFunc(e,"name")}    />
+            <Input type={"text"} placeholder={"Fiyat ekle"} name={"price"} id={"price"} onChange={e=> onChangeFunc(e,"price")}   />
+            <Input type={"file"} placeholder={"Resim seç"}  name={"url"}   id={"url"}   onChange={e=> onChangeFunc(e,"file")}    />
+            <Button btnText={"Oluştur"} onClick={buttonFunc} />
+        </>
+    )
     return (
         <div>
            <ProductCard/>
-           {modal && <Modal title = {"Ürün oluştur"} btnText = {"Oluştur"} btnFunc = {buttonFunc} />} 
+           {modal && <Modal content={contentModal} title = {"Ürün oluştur"} />} 
         </div>
 
         )
